3febbb58a2e23de037c33315b22d51c9bb00c630
S-Najim-S <saidnajimullahs@yahoo.com>
2022-03-31 04:04:58 +0200

    drag and drop functionality added

---
+++ kanban/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainViewComponent } from './components/main-view/main-view.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+@NgModule({
+  declarations: [AppComponent, MainViewComponent],
+  imports: [BrowserModule, AppRoutingModule, DragDropModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ kanban/src/app/components/main-view/main-view.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+@Component({
+  selector: 'app-main-view',
+  templateUrl: './main-view.component.html',
+  styleUrls: ['./main-view.component.scss'],
+})
+export class MainViewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  todos = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  dones = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog',
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+}
